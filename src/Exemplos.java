64e1cbff36c2d72008686cf520aaf295b51d1f4a
Gabriel Harmony <gabrielharmony@hotmail.com>
2020-03-04 20:38:49 -0300

    Exemplos de Método

--- src/Exemplos.java
+++ src/Exemplos.java
@@ -3,14 +3,40 @@ public class Exemplos {
 
 	public static void main(String[] args) {
 
-		int idade = 26;
+		ehMaiorIdade(17);
+		
+		classificar(14);
 
+	}
+	
+	public static void ehMaiorIdade(int idade) {
+		
 		if (idade >= 18) {
 			System.out.println("Maior de Idade");
 		} else {
 			System.out.println("Menor de Idade");
 		}
+		
+	}
+	
+	public static void classificar(int num) {
+		
+		if (num < 0) {
+			
+			System.out.println("Negativo");
+			
+		} else {
+			if (num == 0) {
+				
+				System.out.println("Zero");
+				
+			} else {
 
+				System.out.println("Positivo");
+				
+			}
+		}
+		
 	}
 
 }
