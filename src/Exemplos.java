f3aad1a799931375bc4d2280a87ac636e1f1b243
Gabriel Harmony <gabrielharmony@hotmail.com>
2020-03-04 20:03:39 -0300

    Revisão de IF

---
+++ src/Exemplos.java
@@ -0,0 +1,16 @@
+
+public class Exemplos {
+
+	public static void main(String[] args) {
+
+		int idade = 26;
+
+		if (idade >= 18) {
+			System.out.println("Maior de Idade");
+		} else {
+			System.out.println("Menor de Idade");
+		}
+
+	}
+
+}
